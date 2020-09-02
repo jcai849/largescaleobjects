f2d1152242e8e5b900ce91d2aaeb750e46cedd4a
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-09-03 10:35:51 +1200

    clear out merge issues

--- R/io.R
+++
@@ -1,7 +0,0 @@
-dist.read.csv <- function(files) {
-	fileLoc <- parseFileLoc(files)
-	hosts <- fileLoc$host; filePaths <- fileLoc$filePath
-	nodes <- attainNodes(hosts)
-	distFilePaths <- send(filePaths, nodes)
-	dist.do.call(read.csv, argsDist = list(distFilePaths), argsStatic = list(), assign = TRUE)
-}
--- R/utils.R
+++
@@ -1,8 +0,0 @@
-parseFileLoc <- function(fileLoc) {
-	matchIndices <- regexec("([^:]+):(.*)", fileLoc)
-	matches <- regmatches(fileLoc, matchIndices)
-	splitTable <- do.call(rbind,
-			      lapply(matches, `[`, c(2L, 3L)))
-	colnames(splitTable) <- c("host", "filePath")
-	as.data.frame(splitTable)
-}
