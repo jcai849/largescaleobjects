755a7173d50e70581b5034a16c4915b5b1f14870
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-09-05 12:54:01 +1200

    have step-through for demo

--- demo/chunk-client.R
+++ demo/chunk-client.R
@@ -1,20 +1,31 @@
-#!/usr/bin/env R
-
 library(distObj)
 
+invisible(readline("Type <Return> to continue"))
+# Initialise connections
 distInit()
+
+invisible(readline("Type <Return> to continue"))
+# Clear any previous examples
 rediscc::redis.rm(conn(), c("distChunk1", paste0("C", 1:10), paste0("J", 1:10), 
 		"JOB_ID", "CHUNK_ID"))
+
+invisible(readline("Type <Return> to continue"))
+# Create new example object
 distChunk1 <- structure(new.env(), class = "distChunk")
 chunkID(distChunk1) <- "distChunk1"
 
-main <- function() {
-	cat("Value of distChunk1:", format(distChunk1), "\n")
-	x <- do.call.distChunk(what=expm1, chunkArg=distChunk1, 
-			       assign=T, wait=F)
-	cat("Value of x:", format(x), "\n")
-	y <- do.call.distChunk(log1p, x, assign=T, wait=T)
-	cat("Value of y:", format(y), "\n")
-}
+invisible(readline("Type <Return> to continue"))
+cat("Value of distChunk1:", format(distChunk1), "\n")
 
-main()
+invisible(readline("Type <Return> to continue"))
+x <- do.call.distChunk(what=expm1, chunkArg=distChunk1, 
+		       assign=T, wait=F)
+
+invisible(readline("Type <Return> to continue"))
+cat("Value of x:", format(x), "\n")
+
+invisible(readline("Type <Return> to continue"))
+y <- do.call.distChunk(log1p, x, assign=T, wait=T)
+
+invisible(readline("Type <Return> to continue"))
+cat("Value of y:", format(y), "\n")
