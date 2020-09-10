e1db067be38df79a0790b272218aa3292c3ea679
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-09-10 15:26:45 +1200

    give times for info events

--- R/util.R
+++ R/util.R
@@ -1,1 +1,6 @@
-info <- function(...) if (verbose()) do.call(cat, c("[34m", list(...), "[0m\n"))
+info <- function(...) {
+	op <- options(digits.secs = 6)
+	if (verbose()) do.call(cat, c(format(Sys.time(), "%H:%M:%OS6"), "\t",
+				      "[36m", list(...), "[0m\n"))
+	options(op)
+}
