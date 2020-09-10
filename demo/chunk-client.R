d6a3baaa3246fb6c73d536c0333d34bdc5354987
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-09-10 15:27:49 +1200

    change title colours to be more visible in demo

--- demo/chunk-client.R
+++ demo/chunk-client.R
@@ -13,26 +13,26 @@ resolution(chunk1) <- "RESOLVED"
 
 chunk1
 
-invisible(readline()) ##### [32mAssign Chunk Reference[0m ############################
+invisible(readline()) ##### [44mAssign Chunk Reference[0m #################
 
 x <- do.call.chunkRef(what="expm1", chunkArg=chunk1)
 
-invisible(readline()) ##### [32mAssign Chunk Reference with Failure[0m ###############
+invisible(readline()) ##### [44mAssign Chunk Reference with Failure[0m ####
 
 y <- do.call.chunkRef("as.Date", x)
 
-invisible(readline()) ##### [32mLocal Operations while Chunks Resolve[0m #############
+invisible(readline()) ##### [44mLocal Operations while Chunks Resolve[0m ##
 
 expm1(1:10)
 
-invisible(readline()) ##### [32mPreview of Successful Chunk[0m #######################
+invisible(readline()) ##### [44mPreview of Successful Chunk[0m ############
 
 x
 
-invisible(readline()) ##### [32mValue of Successful Chunk[0m #########################
+invisible(readline()) ##### [44mValue of Successful Chunk[0m ##############
 
 do.call.chunkRef("identity", x, assign=FALSE)
 
-invisible(readline()) ##### [32mResolution of Unsuccessful Chunk[0m ##################
+invisible(readline()) ##### [44mResolution of Unsuccessful Chunk[0m #######
 
 resolve(y)
