eb67e4043058816935497ea0a1f7cf76f0ce3983
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-09-18 13:06:21 +1200

    rename util, shift getLocal to util

--- R/0util.R
+++ R/0util.R
@@ -1,5 +1,5 @@
+getLocal <- function(loc) function(field) get(field, environment(loc))
 envGet <- function(field) function(x) get(field, x)
-
 envSet <- function(field) function(x, value) {
 	assign(field, value, x)
 	x
