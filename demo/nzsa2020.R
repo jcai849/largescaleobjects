abf6318a1b46b1287c290395223be65ad464825c
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-10-30 10:20:27 +1300

    produce outline for demo

--- demo/nzsa2020.R
+++ demo/nzsa2020.R
@@ -5,3 +5,13 @@ flights <- do.call.distObjRef("read.csv",
 				   header	= FALSE,
 				   col.names	= names(cols),
 				   colClasses	= as.vector(cols)))
+
+# anonfun
+
+# table
+
+# lm
+
+# (later) recycling local objects
+
+# ending through a 'quit'
