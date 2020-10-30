b13b476d889739ca29169780b63d7134c57b43f0
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-10-30 13:27:17 +1300

    code filled out for demo

--- demo/nzsa2020.R
+++ demo/nzsa2020.R
@@ -7,11 +7,24 @@ flights <- do.call.distObjRef("read.csv",
 				   colClasses	= as.vector(cols)))
 
 # anonfun
+sanOrig <- do.call.distObjRef(function(df) df$Origin == "SAN", 
+			      list(df = flights))
+
+sanOrigCounts <- emerge(do.call.distObjRef("sum", list(sanOrig)))
+
+sanOrigCount <- sum(sanOrigCounts)
 
 # table
 
+table(flights$Origin, flights$Dest)
+
 # lm
+
+lm(Distance ~ Year, data = flights)
 
 # (later) recycling local objects
 
 # ending through a 'quit'
+
+do.call.distObjRef(function(x) q("no"),
+		   list(x = flights))
