1e4ba8d569e2274cccb455929507ebf8d9ec3026
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-11-02 11:29:11 +1300

    fixes & add dataframe for testing

--- redis-tests/distobj.R
+++ redis-tests/distobj.R
@@ -1,7 +1,7 @@
 source("init.R")
 
-o4		<- 1:15 %% 2 == 0
-distObj4	<- distObj1 %% 2 == 0
+o5		<- 1:15 %% 2 == 0
+distObj5	<- distObj1 %% 2 == 0
 
 # Ops
 stopifnot(
@@ -9,12 +9,12 @@ stopifnot(
 		    emerge(1:3 + distObj1)),
 	  identical(-{1:15}, 			# Unary Arith
 		    emerge(-distObj1)),
-	  identical(o4,				# Comparison
-		    emerge(distObj4)),
-	  identical(o4 | TRUE,		 	# Logical
-		    emerge(distObj4 | TRUE)),
-	  identical(!o4,			# Unary Logic
-		    emerge(!distObj4)),
+	  identical(o5,				# Comparison
+		    emerge(distObj5)),
+	  identical(o5 | TRUE,		 	# Logical
+		    emerge(distObj5 | TRUE)),
+	  identical(!o5,			# Unary Logic
+		    emerge(!distObj5))
 )
 
 # Math
@@ -29,16 +29,16 @@ stopifnot(
 
 # Summary
 stopifnot(
-	  identical(all(o4),
-		    emerge(all(distObj1))),
-	  identical(any(o4),
-		    emerge(any(distObj1))),
+	  identical(all(o5),
+		    all(distObj5)),
+	  identical(any(o5),
+		    any(distObj5)),
 	  identical(sum(1:15),
-		    emerge(sum(distObj1))),
+		    sum(distObj1)),
 	  identical(max(1:15),
-		    emerge(max(distObj1))),
+		    max(distObj1)),
 	  identical(range(1:15),
-		    emerge(range(distObj1)))
+		    range(distObj1))
 )
 
 clear()
--- redis-tests/init.R
+++ redis-tests/init.R
@@ -3,14 +3,15 @@
 ##	distObj1: C1[N1] C2[N2] C3[N3]                                        ##
 ##	distObj2: C4[N2] C5[N3] C6[N1]                                        ##
 ##	distObj3: C7[N2]                                                      ##
+##      distObj4: C8[N1] C9[N2] C10[N3]                                       ##
 ##	                                                                      ##
 ##	 [ N1:9012 ]            [ N2:9013 ]        [ N3:9014 ]                ##
 ##	+--------------------+ +----------------+ +--------------------+      ##
 ##	| C1: 1 2 3 4 5      | | C2: 6 7 8 9 10 | | C3: 11 12 13 14 15 |      ##
 ##	| C6: 11 12 13 14 15 | | C4: 1 2 3 4 5  | | C5: 6 7 8 9 10     |      ##
-##	+--------------------+ | C7: 1 2 3      | +--------------------+      ##
-##	                       +----------------+                             ##
-##	                                                                      ##
+##	|                    | | C7: 1 2 3      | |                    |      ##
+##	| C8: mtcars[1:10,]  | | C9: m[11:20,]  | | C10: m[21:32,]     |      ##
+##	+--------------------+ +----------------+ +--------------------+      ##
 ############################### Fork other nodes ###############################
 
 # N1
@@ -20,6 +21,7 @@ invisible(parallel::mcparallel({
 
 	addChunk("chunk1", 1:5)
 	addChunk("chunk6", 11:15)
+	addChunk("chunk8", mtcars[1:10,])
 
 	server()
 }))
@@ -32,6 +34,7 @@ invisible(parallel::mcparallel({
 	addChunk("chunk2", 6:10)
 	addChunk("chunk4", 1:5)
 	addChunk("chunk7", 1:3)
+	addChunk("chunk9", mtcars[11:20,])
 
 	server()
 }))
@@ -43,6 +46,7 @@ invisible(parallel::mcparallel({
 
 	addChunk("chunk3", 11:15)
 	addChunk("chunk5", 6:10)
+	addChunk("chunk10", mtcars[21:32,])
 
 	server()
 }))
@@ -53,14 +57,21 @@ distInit(verbose=T, osrvPort=9011L, nodeName="N0")
 distObj::clear()
 
 # Create new example object
-chunk1 <- distObj:::makeTestChunk("chunk1", 1:5, port=9012L)
-chunk2 <- distObj:::makeTestChunk("chunk2", 6:10, port=9013L)
-chunk3 <- distObj:::makeTestChunk("chunk3", 11:15, port=9014L)
-chunk4 <- distObj:::makeTestChunk("chunk4", 1:5, port=9013L)
-chunk5 <- distObj:::makeTestChunk("chunk5", 6:10, port=9014L)
-chunk6 <- distObj:::makeTestChunk("chunk6", 11:15, port=9012L)
-chunk7 <- distObj:::makeTestChunk("chunk7", 1:3, port=9013L)
+chunk1	<- distObj:::makeTestChunk("chunk1", 1:5, port=9012L)
+chunk2	<- distObj:::makeTestChunk("chunk2", 6:10, port=9013L)
+chunk3	<- distObj:::makeTestChunk("chunk3", 11:15, port=9014L)
+chunk4	<- distObj:::makeTestChunk("chunk4", 1:5, port=9013L)
+chunk5	<- distObj:::makeTestChunk("chunk5", 6:10, port=9014L)
+chunk6	<- distObj:::makeTestChunk("chunk6", 11:15, port=9012L)
+chunk7	<- distObj:::makeTestChunk("chunk7", 1:3, port=9013L)
+chunk8	<- distObj:::makeTestChunk("chunk8", mtcars[1:10,],
+				  port=9012L, from=1L, to=10L)
+chunk9	<- distObj:::makeTestChunk("chunk9", mtcars[11:20,],
+				  port=9013L, from=11L, to=20L)
+chunk10	<- distObj:::makeTestChunk("chunk10", mtcars[21:32,],
+				  port=9014L, from=21L, to=32L)
 
 distObj1 <- distObj:::makeDistObj(list(chunk1, chunk2, chunk3))
 distObj2 <- distObj:::makeDistObj(list(chunk4, chunk5, chunk6))
 distObj3 <- distObj:::makeDistObj(list(chunk7))
+distObj4 <- distObj:::makeDistObj(list(chunk8, chunk9, chunk10))
