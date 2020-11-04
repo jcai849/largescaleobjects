886c6931be027113a9dcf4d91c37c3e75e5dfd7c
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-11-05 12:06:17 +1300

    add chunk before posting resolution

--- R/server.R
+++ R/server.R
@@ -1,14 +1,14 @@
 server <- function(stopOnError=FALSE) repeat {
 	m <- read.queue(localChunks())
-	v <- if (stopOnError) serveCore(m) else
+	if (stopOnError) serveCore(m) else
 		tryCatch(serveCore(m),
-			 error = function(e) {
-				 print(e)
-				 send(RESOLUTION= "ERROR", 
-				      PREVIEW	= "Error. See frame dump on node", 
-				      to	= postJobID(m))
-				 e})
-	addChunk(postChunkID(m), v)
+		 error = function(e) {
+			 print(e)
+			 addChunk(postChunkID(m), e)
+			 send(RESOLUTION= "ERROR", 
+			      PREVIEW	= "Error. See frame dump on node",
+			      to	= postJobID(m))
+			 NULL})
 }
 
 serveCore <- function(m) {
@@ -17,11 +17,12 @@ serveCore <- function(m) {
 			   target	= target(m),
 			   pCID		= postChunkID(m),
 			   pJID		= postJobID(m))
+	addChunk(postChunkID(m), res)
 	send(RESOLUTION = "RESOLVED", 
 	     PREVIEW 	= preview(res),
 	     SIZE 	= size(res),
 	     HOST	= host(),
 	     PORT	= port(),
 	     to 	= postJobID(m))
-	res
+	NULL
 }
