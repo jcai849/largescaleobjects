88e4e1711fe49fd16d9c4dc684b31bb5faa4eb4c
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-11-04 17:02:13 +1300

    remove names from to's and from's

--- R/distobj.R
+++ R/distobj.R
@@ -48,8 +48,11 @@ resolve.distObjRef <- function(x) {
 	distObjDo(resolve, logical)(x)
 	resolution(x) <- "RESOLVED"
 	tos <- cumsum(size(x))
+	name(tos) <- NULL
 	to(x) <- tos
-	from(x) <- c(1L, tos[-length(chunk(x))] + 1L)
+	froms <- c(1L, tos[-length(chunk(x))] + 1L)
+	name(froms) <- NULL
+	from(x) <- froms
 	T
 }
 
