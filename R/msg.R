6f546e985a38e7c814558fcb0a08553073ddc6f4
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-01-27 13:32:50 +1300

    access methods for msg class

--- R/msg.R
+++ R/msg.R
@@ -1,5 +1,16 @@
-msg <- function(...) {}
-fun.msg <- function() {}
-args.msg <- function() {}
-target.msg <- function() {}
-desc.msg <- function() {}
+msg <- function(...) {
+	m <- list(...)
+	class(m) <- "msg"
+	m
+}
+print.msg <- function(x) {
+	cat("Message with components:\n")
+	for (n in names(x)) {
+		cat("\t", n, ":\n")
+		print(x[[n]])
+	}
+}
+fun.msg <- function(x) x$FUN
+args.msg <- function(x) x$ARGS
+target.msg <- function(x) x$TARGET
+desc.msg <- function(x) x$DESC
