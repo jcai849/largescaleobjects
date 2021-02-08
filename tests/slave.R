297be30553e2735e50ed53cee8bd740cc43d5b4a
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-02-09 12:59:04 +1300

    remove legacy tests

--- tests/slave.R
+++
@@ -1,9 +0,0 @@
-largeScaleR::worker(comms=structure(list(host='localhost', port=6379L, user=NULL,
-					 pass=NULL, dbpass=NULL),
-				    class=c("commsProcess", "process")), 
-		    host='localhost',
-		    desc=largeScaleR::desc("process"),
-		    port=largeScaleR::port(),
-		    stopOnError=TRUE,
-		    verbose=TRUE)
-
