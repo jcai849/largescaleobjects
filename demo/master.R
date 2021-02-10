8bdbbed62542c57908e4e714f10bcfba2372f5b7
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-02-11 08:51:11 +1300

    add demos

---
+++ demo/master.R
@@ -0,0 +1,19 @@
+#!/usr/local/bin/Rscript --vanilla
+
+library(largeScaleR)
+
+commsProcess('localhost')
+userProcess('localhost')
+
+fileLoc <- tempfile(fileext=".csv")
+data <- mtcars
+write.csv(data, fileLoc, row.names=FALSE)
+
+x <- read(localCSV(fileLoc), col_types = sapply(data, class))
+
+debug(largeScaleR:::resolve.distObjStub)
+
+print(x)
+print(x$mpg * x$cyl)
+
+.Last()
---
+++ demo/worker.R
@@ -0,0 +1,12 @@
+#!/usr/local/bin/Rscript --vanilla
+
+debug(largeScaleR::worker)
+
+largeScaleR::worker(comms=structure(list(host='localhost', port=6379L, user=NULL,
+					 pass=NULL, desc="comms", dbpass=NULL),
+				    class=c("commsProcess", "process")), 
+		    host='localhost',
+		    port=largeScaleR::port(),
+		    desc=largeScaleR::desc("process"),
+		    stopOnError=TRUE,
+		    verbose=TRUE)
