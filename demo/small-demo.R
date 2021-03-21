3a5403b454aea78b60fa5de2be9fc965c2bef073
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-03-22 10:42:12 +1300

    functional but slow - profiling added

--- demo/small-demo.R
+++ demo/small-demo.R
@@ -1,23 +1,31 @@
 library(largeScaleR)
+options(keep.source=TRUE,
+	keep.source.pkgs=TRUE)
 
 init("config")
 
 # make data
-
-nrow <- 1E6
-nCases <- rbinom(1E6, 20, 0.3)
+nrow <- 1E5
+nCases <- rbinom(1E5, 20, 0.3)
 COVID <- data.frame(date = as.character(sample(seq(as.Date("2020-01-01"),
-						   Sys.Date(), by="day"), 1E6,
+						   Sys.Date(), by="day"), 1E5,
 					       replace=TRUE)),
-                   loc = sample(replicate(100, paste0(sample(letters, 3), collapse="")), 1E6, replace=TRUE),
-                   lockdowns = rbinom(1E6, 4, 0.2))
+                   loc = sample(replicate(100, paste0(sample(letters, 3), collapse="")), 1E5, replace=TRUE),
+                   lockdowns = rbinom(1E5, 4, 0.2))
 cols <- sapply(COVID, class)
 fileLoc <- tempfile(pattern="largeScaleR", fileext=".csv")
 write.table(COVID, fileLoc,, F, ",",, "",,F,F)
 CSVfile <- localCSV(loc=fileLoc, colTypes=cols,header=FALSE,quote="")
 
 # distribute data
-distCOVID <- read(CSVfile, max.size=1024^2) #10Mb
-cache(distCOVID)
+Rprof(filename="master.out",
+      append=TRUE,
+      memory.profiling=TRUE,
+      line.profiling=TRUE,
+      filter.callframes=TRUE)
+distCOVID <- read(CSVfile, max.size=100*1024) #100Kb
 distnCases <- stub(nCases, 10)
-cache(distnCases)
+k=distCOVID$lockdowns * distnCases
+preview(k)
+final(1)
+Rprof(NULL)
