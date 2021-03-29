149a0b1854830a5ebfc2c89e57b1ab7d7cf9a22f
Simon Urbanek <simon.urbanek@R-project.org>
2021-03-29 20:37:14 +0000

    init multi-node demo

---
+++ demo/multi-demo.R
@@ -0,0 +1,24 @@
+library(largeScaleR)
+options(keep.source=TRUE,
+	keep.source.pkgs=TRUE)
+
+init("config-multi")
+
+# make data
+nrow <- 1E5
+nCases <- rbinom(1E5, 20, 0.3)
+COVID <- data.frame(date = as.character(sample(seq(as.Date("2020-01-01"),
+						   Sys.Date(), by="day"), 1E5,
+					       replace=TRUE)),
+                   loc = sample(replicate(100, paste0(sample(letters, 3), collapse="")), 1E5, replace=TRUE),
+                   lockdowns = rbinom(1E5, 4, 0.2))
+cols <- sapply(COVID, class)
+fileLoc <- tempfile(pattern="largeScaleR", fileext=".csv")
+write.table(COVID, fileLoc,, F, ",",, "",,F,F)
+CSVfile <- localCSV(loc=fileLoc, colTypes=cols,header=FALSE,quote="")
+
+# distribute data
+distCOVID <- read(CSVfile, max.size=100*1024) #100Kb
+distnCases <- stub(nCases, 18)
+k=distCOVID$lockdowns * distnCases
+preview(k)
