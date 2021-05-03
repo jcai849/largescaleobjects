d6961b3eab6d3fbb1cacc5dc5a32d2ab5b01a8f7
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-05-03 17:51:31 +1200

    initial distributed URL reading support

--- R/in.R
+++ R/in.R
@@ -1,32 +1,46 @@
-distributedCSV <- function(host, file, header, colClasses, quotes) {
-	x <- list()
+distributedCSV <- function(files, header, colClasses, quotes) {
+	x <- list(host=character(length(files)),
+		  path=character(length(files)),
+		  header=character(10),
+		  colclasses=character(10),
+		  quotes=character(1))
 	class(x) <- "distributedCSV"
-	host(x) <- host
-	file(x) <- file
+	uri <- regmatches(x, regexec("^(?<host>.*?):(?<path>.*)", x, perl=T))
+	for (i in seq(length(uri))) {
+		if (length(uri[[i]]) == 3) {#host found
+			host(x)[i] <- uri[2]
+			path(x)[i] <- uri[3]
+		} else {
+			host(x)[i] <- NULL
+			path(x)[i] <- files
+		}
+	}
 	colClasses(x) <- colClasses
 	header(x) <- header
 	quotes(x) <- quotes
 	x
 }
 
-read.distributedCSV <- function(dcsv)
-	distObjRef(lapply(dcsv, function(x)
+read.distributedCSV <- function(dcsv) {
+	distObjRef(mapply(function(x, host, path)
 			  do.call.chunkRef("read.csv", 
-					   args = list(file=I(file(x)),
+					   args = list(file=I(path),
 						       header = I(header(x)),
 						       colClasses = I(colClasses(x)), 
 						       quote = I(quotes(x))), 
-					   target = x)))
+					   target = if (is.null(host)) root() else host),
+			  x=dcsv, host=host(x), path=path(x)))
+}
 
 
-file.distributedCSV <- function(x, ...) x$file
 host.distributedCSV <- function(x, ...) x$host
+path.distributedCSV <- function(x, ...) x$path
 desc.distributedCSV <- host
 colClasses.distributedCSV <- function(x, ...) x$colClasses
 header.distributedCSV <- function(x, ...) x$header
 quotes.distributedCSV <- function(x, ...) x$quotes
-`file<-.distributedCSV` <- function(x, value) {x$file <- value; x}
 `host<-.distributedCSV` <- function(x, value) {x$host <- value; x}
+`path<-.distributedCSV` <- function(x, value) {x$file <- value; x}
 `colClasses<-.distributedCSV` <- function(x, value) {x$colClasses <- value; x}
 `header<-.distributedCSV` <- function(x, value) {x$header <- value; x}
 `quotes<-.distributedCSV` <- function(x, value) {x$quotes <- value; x}
