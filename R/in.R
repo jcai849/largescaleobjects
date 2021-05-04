99392e6b72b4709027e4559e52ec9994531ba034
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-05-05 11:27:47 +1200

    light taxicab demo functioning

--- R/in.R
+++ R/in.R
@@ -6,19 +6,22 @@ distributedCSV <- function(files, header, col.names, colClasses, quotes="\"") {
 		  colclasses=character(1),
 		  quotes=character(1))
 	class(x) <- "distributedCSV"
-	uri <- regmatches(x, regexec("^(?<host>.*?):(?<path>.*)", x, perl=T))
-	for (i in seq(length(uri))) {
-		if (length(uri[[i]]) == 3 && 
-		    !grepl("^(https?)|(ftp)$", uri[2])) {# non-internet host found
-			host(x)[i] <- uri[2]
-			path(x)[i] <- uri[3]
-		} else {
-			host(x)[i] <- NA_character_
-			path(x)[i] <- files
-		}
-	}
+	path(x) <- files
+	host(x) <- NA_character_
+	# TODO: allow files on networked machines
+#	uri <- regmatches(x, regexec("^(?<host>.*?):(?<path>.*)", x, perl=T))
+#	for (i in seq(length(uri))) {
+#		if (length(uri[[i]]) == 3 && 
+#		    !grepl("^(https?)|(ftp)$", uri[2])) {# non-internet host found
+#			host(x)[i] <- uri[2]
+#			path(x)[i] <- uri[3]
+#		} else {
+#			host(x)[i] <- NA_character_
+#			path(x)[i] <- files
+#		}
+#	}
 	colClasses(x) <- colClasses
-	colNames(x) <- colClasses
+	colNames(x) <- col.names
 	header(x) <- header
 	quotes(x) <- quotes
 	x
@@ -26,19 +29,20 @@ distributedCSV <- function(files, header, col.names, colClasses, quotes="\"") {
 
 read.distributedCSV <- function(dcsv) {
 	distObjRef(mapply(function(file, header, colClasses, col.names, quotes, target) {
-				  browser()
 				  do.call.chunkRef("read.csv", 
 						   args = list(file=I(file),
 							       header = I(header),
 							       colClasses = I(colClasses), 
-							       col.names = I(colNames), 
+							       col.names = I(col.names), 
 							       quote = I(quotes)), 
-						   target = if (is.na(target)) root() else 
-							   structure(list(host=target), class="distributedCSV"))
+						   target = root()) #TODO change for networked csv
 				},
-				file=path(dcsv), header=header(dcsv),
-				colClasses=colClasses(dcsv), col.names=colNames(dcsv), 
-				quotes=quotes(dcsv), target=host(dcsv)))
+				file=path(dcsv), target=host(dcsv), 
+				MoreArgs = list(header=header(dcsv),
+						colClasses=colClasses(dcsv),
+						col.names=colNames(dcsv), 
+						quotes=quotes(dcsv)),
+			  SIMPLIFY=FALSE, USE.NAMES=FALSE))
 }
 
 host.distributedCSV <- function(x, ...) x$host
@@ -49,14 +53,13 @@ colNames.distributedCSV <- function(x, ...) x$col.names
 header.distributedCSV <- function(x, ...) x$header
 quotes.distributedCSV <- function(x, ...) x$quotes
 `host<-.distributedCSV` <- function(x, value) {x$host <- value; x}
-`path<-.distributedCSV` <- function(x, value) {x$file <- value; x}
+`path<-.distributedCSV` <- function(x, value) {x$path <- value; x}
 `colClasses<-.distributedCSV` <- function(x, value) {x$colClasses <- value; x}
 `colNames<-.distributedCSV` <- function(x, value) {x$col.names <- value; x}
 `header<-.distributedCSV` <- function(x, value) {x$header <- value; x}
 `quotes<-.distributedCSV` <- function(x, value) {x$quotes <- value; x}
 print.distributedCSV <- function(x, ...) 
-	cat("distributedCSV with file path", paste(path(x), collapse="; "), 
-	    if (!is.na(host(x))) c("at host", paste(host(x), collapse="; ")) else NULL, "\n")
+	cat("distributedCSV with file path", paste(path(x), collapse="; "), "\n")
 format.distributedCSV <- function(x, ...) paste("distributedCSV with file location", path(x))
 
 localCSV <- function(file, header, colTypes, quotes="") {
--- demo/taxicab.R
+++ demo/taxicab.R
@@ -1,7 +1,27 @@
-library(largeScaleR)
-#init("config-single")
-cols <- c("VendorID"="integer","tpep_pickup_datetime"="POSIXct","tpep_dropoff_datetime"="POSIXct","passenger_count"="integer","trip_distance"="numeric","RatecodeID"="integer","store_and_fwd_flag"="character","PULocationID"="integer","DOLocationID"="integer","payment_type"="integer","fare_amount"="numeric","extra"="numeric","mta_tax"="numeric","tip_amount"="numeric","tolls_amount"="numeric","improvement_surcharge"="numeric","total_amount"="numeric","congestion_surcharge"="numeric")
-files <- "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-01.csv"
-dcsv <- distributedCSV(files, header=T, col.names=names(cols), colClasses=cols)
-#x <- read.csv(files, header=TRUE, col.names=names(cols), colClasses=cols)
 #https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+
+library(largeScaleR)
+init("config")
+files <- apply(expand.grid("https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_20",
+			  formatC(9:20, width=2,flag=0), "-", formatC(1:12, width=2, flag=0), 
+			  ".csv"),
+	      1, paste0, collapse="")
+cols <- c("VendorID"="integer","tpep_pickup_datetime"="POSIXct","tpep_dropoff_datetime"="POSIXct","passenger_count"="integer","trip_distance"="numeric","RateCodeID"="integer","store_and_fwd_flag"="character","PULocationID"="integer","DOLocationID"="integer","payment_type"="integer","fare_amount"="numeric","extra"="numeric","mta_tax"="numeric","tip_amount"="numeric","tolls_amount"="numeric","improvement_surcharge"="numeric","total_amount"="numeric","congestion_surcharge"="numeric")
+
+taxi.dcsv <- distributedCSV(files[143:144], header=T, col.names=names(cols), colClasses=as.vector(cols))
+taxicab <- read(taxi.dcsv)
+print(taxicab)
+preview(taxicab)
+nrow(taxicab)
+desc(taxicab)
+host(taxicab)
+
+isCMT <- taxicab$VendorID == 1L
+print(isCMT)
+preview(isCMT)
+sum(isCMT)
+CMT <- subset(taxicab, isCMT)
+length(CMT)
+
+passengerLoc <- table(CMT$passenger_count, CMT$RateCodeID)
+print(passengerLoc)
