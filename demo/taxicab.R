4e2c1e9e31f3a16d71a7b683a490fcfef9fbd87f
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-05-05 13:55:32 +1200

    order files chronologically in taxi demo

--- demo/taxicab.R
+++ demo/taxicab.R
@@ -2,13 +2,29 @@
 
 library(largeScaleR)
 init("config")
+START_YEAR <- 9
+N_YEARS <- 11
+N_MONTHS <- 12
 files <- apply(expand.grid("https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_20",
-			  formatC(9:20, width=2,flag=0), "-", formatC(1:12, width=2, flag=0), 
+			  formatC(seq(START_YEAR, START_YEAR+N_YEARS), width=2,flag=0), 
+			  "-", formatC(seq(N_MONTHS), width=2, flag=0), 
 			  ".csv"),
-	      1, paste0, collapse="")
-cols <- c("VendorID"="integer","tpep_pickup_datetime"="POSIXct","tpep_dropoff_datetime"="POSIXct","passenger_count"="integer","trip_distance"="numeric","RateCodeID"="integer","store_and_fwd_flag"="character","PULocationID"="integer","DOLocationID"="integer","payment_type"="integer","fare_amount"="numeric","extra"="numeric","mta_tax"="numeric","tip_amount"="numeric","tolls_amount"="numeric","improvement_surcharge"="numeric","total_amount"="numeric","congestion_surcharge"="numeric")
+	      1, paste0, collapse="")[sapply(seq(N_YEARS+1),
+				      function(i) seq(i,
+						      (1+N_YEARS)*N_MONTHS,
+						      N_YEARS+1))]
+cols <- c("VendorID"="integer", "tpep_pickup_datetime"="POSIXct",
+	  "tpep_dropoff_datetime"="POSIXct", "passenger_count"="integer",
+	  "trip_distance"="numeric", "RateCodeID"="integer",
+	  "store_and_fwd_flag"="character", "PULocationID"="integer",
+	  "DOLocationID"="integer", "payment_type"="integer",
+	  "fare_amount"="numeric", "extra"="numeric", "mta_tax"="numeric",
+	  "tip_amount"="numeric", "tolls_amount"="numeric",
+	  "improvement_surcharge"="numeric", "total_amount"="numeric",
+	  "congestion_surcharge"="numeric")
 
-taxi.dcsv <- distributedCSV(files[143:144], header=T, col.names=names(cols), colClasses=as.vector(cols))
+taxi.dcsv <- distributedCSV(files[143:144], header=T, col.names=names(cols),
+			    colClasses=as.vector(cols))
 taxicab <- read(taxi.dcsv)
 print(taxicab)
 preview(taxicab)
