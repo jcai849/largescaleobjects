ee86a736c3debf0788a73b68df12994d968cae92
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-05-12 11:48:44 +1200

    functioning first 2 demos

--- demo/prop-1.R
+++ demo/prop-1.R
@@ -1,9 +1,11 @@
 library(largeScaleR)
-debug(start)
-start(workers=paste0("10.3.1.", c(17, 2, 5, 3, 15, 8, 7, 9)), 
-      user="10.3.1.1")
+workers <- paste0("10.3.1.", c(17, 2, 5, 3, 15, 8, 7, 9)) 
 
-lx <- rnorm(1E3)
+start(workers=workers, user="10.3.1.1")
+
+lx <- rnorm(1E6)
+head(lx)
+
 dx <- distribute(lx, 8)
 print(dx)
 preview(dx)
--- demo/prop-2.R
+++ demo/prop-2.R
@@ -1,8 +1,27 @@
-splx <- split(lx, cut(seq(length(lx)), breaks=8))
-sum_map <- do.call("sum", splx)
-reduced <- sum(sum_map)
+# Local
+###### SPLIT
+lsplit <- split(lx, cut(seq(length(lx)), breaks=8))
+str(lsplit)
+###### MAP
+lmap <- lapply(lsplit, sum)
+str(lmap)
+###### EMERGE
+lemerged <- as.vector(do.call("c", lmap))
+print(lemerged)
+###### REDUCE
+lreduced <- sum(lemerged)
+print(lreduced)
 
-sum_dmap <- do.dcall("sum", dx)
-dreduced <- sum(emerge(sum_dmap))
+# Distributed
+###### MAP
+dmap <- do.dcall("sum", list(dx))
+preview(dmap)
+###### EMERGE
+demerged <- emerge(dmap)
+print(demerged)
+###### REDUCE
+dreduced <- sum(demerged)
+print(dreduced)
 
+###### ALL-IN-ONE
 sum(dx)
