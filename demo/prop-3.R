6795692c1720d342e586a985c2e53bc65663beda
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-05-13 14:59:21 +1200

    change demo for old taxi data

--- demo/prop-3.R
+++ demo/prop-3.R
@@ -1,18 +1,11 @@
 source("prop-3-setup")
+
 taxicab <- read.dlcsv(hosts, files)
 print(taxicab)
 preview(taxicab)
 nrow(taxicab)
-desc(taxicab)
-host(taxicab)
+object.dsize(taxicab)
 
 isCMT <- taxicab$VendorID == 1L
-print(isCMT)
 preview(isCMT)
 sum(isCMT, na.rm=T)
-CMT <- subset(taxicab, isCMT)
-length(CMT)
-
-passengertip <- table(CMT$passenger_count, CMT$tip_amount)
-print(passengertip)
-col(passengertip)
