3679e118734a02c2bc35947550634babc1752596
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-05-15 21:13:21 +1200

    reflect start function in demo

--- demo/prop-3.R
+++ demo/prop-3.R
@@ -4,7 +4,6 @@ taxicab <- read.dlcsv(host, file, col.names=names(cols), colClasses=cols)
 print(taxicab)
 preview(taxicab)
 nrow(taxicab)
-object.dsize(taxicab)
 
 totaltips <- taxicab$tip_amount
 sum(totaltips, na.rm=T)
