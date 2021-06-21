83c972be6a1dceb605e6517970f5a0e344d180bd
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-06-21 14:33:16 +1200

    have model function with masking

--- R/model.R
+++ R/model.R
@@ -1,16 +1,17 @@
 dlm <- function(formula, data, weights=NULL, sandwich=FALSE) {
         stopifnot(is.distObjRef(data))
-        chunks <- chunkRef(distObjRef)
+        chunks <- chunkRef(data)
         stopifnot(length(chunks) > 0L)
+	currcall <- sys.call()
+	scform <- c(alist(...=), call("quote", currcall))
 	init <- do.ccall("biglm::biglm",
 			 list(formula=stripEnv(formula),
 			      data=chunks[[1]],
 			      weights=stripEnv(weights),
 			      sandwich=sandwich),
 			 target=chunks[[1]],
-			 mask=list(sys.call=c(alist(...=),
-					      call("quote", sys.call()))))
+			 mask=list(sys.call=scform))
         if (length(chunks) != 1L)
-                dreduce("biglm::update.biglm", chunks[-1], init)
+                dreduce("biglm::update.biglm", distObjRef(chunks[-1]), init)
         else init
 }
