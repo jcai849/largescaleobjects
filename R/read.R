6bf915af89dce3f3e8059e9067e3c4eb1c81841e
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-03-02 12:55:47 +1300

    specify rbind combination for dcsv

--- R/read.R
+++ R/read.R
@@ -1,4 +1,4 @@
 read.dcsv <- function(hosts, paths) {
 	chunks <- mapply(largerscale::push, paths, hosts)
-	do.dcall(read.csv, structure(list(chunks=chunks), class="DistributedObject")
+	do.dcall(read.csv, structure(list(chunks=chunks), class="DistributedObject"), combination=rbind)
 }
