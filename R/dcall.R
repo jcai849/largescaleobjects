24e9cb889a2fcdefe6f6d5f939b7c200ca6b29e1
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-03-02 11:54:28 +1300

    simplify dcall to enable alignment later

--- R/dcall.R
+++ R/dcall.R
@@ -1,8 +1,5 @@
 do.dcall <- function(what, args, size_change=FALSE) {
-	dist_obj_args <- sapply(args, inherits, "DistributedObject")
-	args[!dist_obj_args] <- lapply(args[!dist_obj_args], split, args[dist_obj_args][1])
-	chunks <- lapply(seq(length(args[[1]]$chunks)),
-			 function(i) largerscale::remote_call(what,
-							      lapply(args, function(arg) arg$chunks[[i]]))
+	aligned <- lapply(args, function(arg) if (inherits(arg, "DistributedObject") unclass(arg)$chunks else arg)
+	chunks <- do.call(mapply, list(function(...) largerscale::remote_call(what, list(...)), aligned))
 	structure(list(chunks=chunks, size=NA_integer_, combination=c), class="DistributedObject")
 }
