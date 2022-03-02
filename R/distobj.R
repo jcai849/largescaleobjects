7e78b2da7e77a18f18b04aa5ee2d5b6b4077972c
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-03-02 13:01:38 +1300

    add table combination method

--- R/distobj.R
+++ R/distobj.R
@@ -21,7 +21,7 @@ Summary.DistributedObject <- function(..., na.rm = FALSE) {
 	do.dcall("$", list(x=x, name=I(name)))
 
 table.DistributedObject <- function(...)
-	emerge(do.dcall("table", list(...)))
+	emerge(do.dcall("table", list(...), combination=combine.table))
 
 subset.DistributedObject <- function(x, subset, ...)
 	do.dcall("subset", c(list(x=x, subset=subset), list(...)))
@@ -34,3 +34,25 @@ dim.DistributedObject <- function(x) {
 size <- function(measure) function(x) sum(do.dcall(measure, x))
 length.DistributedObject <- size(length)
 nrow.DistributedObject <- size(nrow)
+
+combine.table <- function(...) {
+	tabs <- list(...)
+	chunknames <- lapply(tabs, dimnames)
+	stopifnot(all(lengths(chunknames) == length(chunknames[[1]])))
+	groupedvarnames <- lapply(seq(length(chunknames[[1]])),
+			      function(i) lapply(chunknames,
+						 function(chunk) chunk[[i]]))
+	wholenames <- structure(lapply(groupedvarnames,
+		       function(names) sort(unique(do.call(c, names)))),
+			  names = names(chunknames[[1]]))
+	wholearray <- array(0L, dim = lengths(wholenames, use.names = FALSE),
+			    dimnames = wholenames)
+	lapply(seq(length(tabs)), function(i)
+	       {eval(substitute(wholearray_sub <<- wholearray_sub + tab_chunk, 
+		   list(wholearray_sub =  do.call(call, c(list("["),
+			x = quote(quote(wholearray)),
+			unname(chunknames[[i]]))),
+			tab_chunk = substitute(tabs[[i]], list(i = i)))))
+	NULL})
+	as.table(wholearray)
+}
