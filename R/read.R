5ddb8d2549af4e7cc5907c55b13406fbc4f6967a
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-03-02 13:15:26 +1300

    pass forward all read.csv args to dcsv

--- R/read.R
+++ R/read.R
@@ -1,4 +1,16 @@
-read.dcsv <- function(hosts, paths) {
-	chunks <- mapply(largerscale::push, paths, hosts)
-	do.dcall(read.csv, structure(list(chunks=chunks), class="DistributedObject"), combination=rbind)
+read.dcsv <- function(hosts, paths, header=FALSE, sep=",", quote="\"",
+		      dec=".", fill=TRUE, comment.char="", col.names, colClasses) {
+	chunks <- mapply(largerscale::push, paths, hosts,
+			 SIMPLIFY=FALSE, USE.NAMES=FALSE)
+	do.dcall(read.csv,
+		 list(file=structure(list(chunks=chunks), class="DistributedObject"),
+		      header=header,
+		      sep=sep,
+		      quote=quote,
+		      dec=dec,
+		      fill=fill,
+		      comment.char=comment.char,
+		      col.names=col.names,
+		      colClasses=colClasses),
+		 combination=rbind)
 }
