b298c4fce0f215afa36a28d7cd8ec0746e629bbf
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-03-28 13:20:55 +1300

    fix init

--- R/init.R
+++ R/init.R
@@ -1,17 +1,17 @@
 init_locator <- function(host, port) {
-	LOCATOR(host, port)
-	remote_sys(host, "largescaler::locator", list(host, port))
+	largerscale::LOCATOR(host, port)
+	remote_sys(host, "largerscale::locator", list(host, port))
 }
 
 init_worker <- function(host, port) {
-	locator_address <- LOCATOR()$address
-	locator_port <- LOCATOR()$port
+	locator_address <- largerscale::LOCATOR()$address
+	locator_port <- largerscale::LOCATOR()$port
 	if (is.null(locator_address) || is.null(locator_port))
 		stop("Location service not yet initialised")
-	remote_sys(host, "largescaler::worker", list(host, port, locator_address, locator_port))
+	remote_sys(host, "largerscale::worker", list(host, port, locator_address, locator_port))
 }
 
 remote_sys <- function(host, fun, args) {
-	command <- c("R", "-e", paste0(fun, "(", do.call(paste, c(args, sep=", ")), ")" ))
+	command <- c("R", "-e", as.call(c(parse(text=fun), args)))
 	system2("ssh", c(host, shQuote(shQuote(command))), stdout=FALSE, stderr=FALSE, wait=FALSE)
 }
