265ceef86a614d7b3f767ef24f55b05a6f61d49a
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-04-06 08:36:22 +1200

    error on singular distributed object given as arg to dcall

--- R/dcall.R
+++ R/dcall.R
@@ -1,4 +1,5 @@
 do.dcall <- function(what, args, size_change=FALSE) {
+	if (inherits(args, "DistributedObject")) stop("Requires list for argument, not distributed object")
 	aligned <- lapply(args, function(arg) if (inherits(arg, "DistributedObject")) unclass(arg)$chunks else arg)
 	chunks <- do.call(mapply, c(list(function(...) largerscale::remote_call(what, list(...))),
 				    aligned,
