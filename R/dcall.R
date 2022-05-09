89b5130d43fb34800dec2f55003c2ecf52ad8b1f
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-05-09 14:55:15 +1200

    create "d" wrapper for dcall

--- R/dcall.R
+++ R/dcall.R
@@ -6,3 +6,6 @@ do.dcall <- function(what, args, size_change=FALSE) {
 				    list(SIMPLIFY=FALSE, USE.NAMES=FALSE)))
 	structure(list(chunks=chunks, size=NA_integer_), class="DistributedObject")
 }
+
+d <- function(what, size_change=FALSE) function(...) do.dcall(what, args=list(...), size_change=size_change)
+# e.g. d.model.matrix <- d(model.matrix); d.model.matrix(object=~ a + b, dd)
