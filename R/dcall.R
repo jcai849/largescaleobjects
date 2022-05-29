2b1ffa122888290aaa396b61c822b2fd357f42f1
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-05-30 11:50:59 +1200

    remove size considerations

--- R/dcall.R
+++ R/dcall.R
@@ -1,11 +1,11 @@
-do.dcall <- function(what, args, size_change=FALSE) {
+do.dcall <- function(what, args) {
 	if (inherits(args, "DistributedObject")) stop("Requires list for argument, not distributed object")
 	aligned <- lapply(args, function(arg) if (inherits(arg, "DistributedObject")) unclass(arg)$chunks else arg)
 	chunks <- do.call(mapply, c(list(function(...) largerscale::remote_call(what, list(...))),
 				    aligned,
 				    list(SIMPLIFY=FALSE, USE.NAMES=FALSE)))
-	structure(list(chunks=chunks, size=NA_integer_), class="DistributedObject")
+	structure(list(chunks=chunks), class="DistributedObject")
 }
 
-d <- function(what, size_change=FALSE) function(...) do.dcall(what, args=list(...), size_change=size_change)
+d <- function(what) function(...) do.dcall(what, args=list(...))
 # e.g. d.model.matrix <- d(model.matrix); d.model.matrix(object=~ a + b, dd)
