ca4e56d9ff8f4213ada2193c8c797a897ffa68f0
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-05-30 15:14:42 +1200

    return distobj with reduce

--- R/reduce.R
+++ R/reduce.R
@@ -1,5 +1,5 @@
 dReduce <- function(f, x, init, right = FALSE, accumulate = FALSE, ...) {
-        Reduce(dreducable(f, ...), as.list(x), init, right, accumulate)
+        DistributedObject(Reduce(dreducable(f, ...), as.list(x), init, right, accumulate))
 }
 
 dreducable <- function(f, ...) {
