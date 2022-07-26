2c166c0c2650779e8e42de0d403dbf89fd8f8732
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-07-26 16:01:07 +1200

    remote call operating with bulk requests

--- R/dcall.R
+++ R/dcall.R
@@ -1,9 +1,10 @@
 do.dcall <- function(what, args) {
 	if (inherits(args, "DistributedObject")) stop("Requires list for argument, not distributed object")
 	aligned <- lapply(args, function(arg) if (inherits(arg, "DistributedObject")) as.list(arg) else arg)
-	chunks <- do.call(mapply, c(list(function(...) chunknet::remote_call(what, list(...))),
+	chunks <- do.call(mapply, c(list(function(...) chunknet::remote_call(what, list(...), post_locs=FALSE)),
 				    aligned,
 				    list(SIMPLIFY=FALSE, USE.NAMES=FALSE)))
+	post_locations(sapply(chunk, '$', "href"), sapply(chunk, '$', "init_loc"))
 	DistributedObject(chunks)
 }
 
