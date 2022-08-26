2ce4dabad228f939223e41159f86a572d8a30021
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-08-27 09:41:49 +1200

    seperate action of enlisting from aligning for clarity

--- R/dcall.R
+++ R/dcall.R
@@ -1,8 +1,7 @@
 do.dcall <- function(what, args) {
 	if (inherits(args, "DistributedObject")) stop("Requires list for argument, not distributed object")
-	aligned <- do.call(mapply, c(list,
-					lapply(args, function(arg) if (inherits(arg, "DistributedObject")) as.list(arg) else arg),
-					SIMPLIFY=FALSE))
+	enlisted_args <- lapply(args, function(arg) if (inherits(arg, "DistributedObject")) as.list(arg) else arg),
+	aligned <- do.call(mapply, c(list, enlisted_args, SIMPLIFY=FALSE))
 	chunks <- chunknet::do.ccall(list(what), aligned)
 	DistributedObject(unlist(chunks, recursive=FALSE))
 }
