5891b0d811d4c49f6da68199405b0e7d7787594b
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-08-31 16:26:19 +1200

    allow compilation

--- R/dcall.R
+++ R/dcall.R
@@ -1,7 +1,7 @@
 do.dcall <- function(what, args) {
 	if (inherits(args, "DistributedObject")) stop("Requires list for argument, not distributed object")
-	enlisted_args <- lapply(args, function(arg) if (inherits(arg, "DistributedObject")) as.list(arg) else arg),
-	aligned <- do.call(mapply, c(list, enlisted_args, SIMPLIFY=FALSE))
+	enlisted_args <- lapply(args, function(arg) if (inherits(arg, "DistributedObject")) as.list(arg) else arg)
+	aligned <- do.call(mapply, c(list, enlisted_args, SIMPLIFY=FALSE, USE.NAMES=FALSE))
 	chunks <- chunknet::do.ccall(list(what), aligned)
 	DistributedObject(unlist(chunks, recursive=FALSE))
 }
