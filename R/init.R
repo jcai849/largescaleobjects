c947008350531ce4a05576583fa379d95c973ba1
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-10-03 13:18:16 +1300

    use correct names for init functions

--- R/init.R
+++ R/init.R
@@ -1,12 +1,12 @@
 init_locator <- function(host, port) {
 	chunknet::LOCATOR(host, port)
-	remote_sys(host, "chunknet::locator", list(host, port))
+	remote_sys(host, "chunknet::locator_node", list(host, port))
 }
 
 init_worker <- function(host, port) {
 	if (is.null(chunknet::LOCATOR()))
 		stop("Location service not yet initialised")
-	remote_sys(host, "chunknet::worker",
+	remote_sys(host, "chunknet::worker_node",
 		   list(host, port,
 			orcv::address(chunknet::LOCATOR()), orcv::port(chunknet::LOCATOR())))
 }
