759ad38b14e7920269d8d2eb76fa4a8cf2d8d1ad
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-12-07 08:53:29 +1300

    clarify comments on pieces

--- R/piece.R
+++ R/piece.R
@@ -3,11 +3,11 @@ index.numeric <- function(x, i, j, ...) {
 	stopifnot(is.DistributedObject(x))
 	sizes <- emerge(do.dcall(dim, list(x)))
 	cumsum(sizes)
-	# Actually this cumsum should be a generic that depends on the dimension and class
+	# What if arrays aren't combined end-to-end, as in distributed matrix?
+	# It seems that this cumsum should be a method that depends on the dimension and class
 	# e.g.
-	# > x <- array(1:9, c(3,3))
-	# > apply(sapply(list(x,x,x,x), dim), 1, cumsum)
-	# Question: what if arrays aren't combined end-to-end?
+	#> x <- array(1:9, c(3,3))
+	#> apply(sapply(list(x,x,x,x), dim), 1, cumsum)
 }
 prune <- function(x)
 align <- function(x, y)
