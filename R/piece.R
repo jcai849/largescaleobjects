61540e6669d28bbf2b5e5bf3135d6b08808f5f6c
Jason Cairns <jcai849@aucklanduni.ac.nz>
2022-12-13 09:55:16 +1300

    develop indexing with better clarity

--- R/piece.R
+++ R/piece.R
@@ -4,21 +4,30 @@ index.numeric <- function(x, i, ...) {
 	x <- chunkReferenceArray(x)
 	chunk_dims <- emerge(d(dim)(x))
 	stopifnot(identical(dim(chunk_dims), dim(x)))
-	margins <- seq(length(dim(dims))))
-	
-	accum_dims <- lapply(margins, function(margin) {
-		cumsum(apply(chunk_dims, margin, function(s) apply(s, margin, sum)))
-	})
-	gi <- gen_indices(i, accum_dims)
+
+	gi <- gen_indices(i, accumulated_dims(chunk_dims))
 	stopifnot(identical(dim(gi), dim(x)))
 	
-	DistributedObject(send_indices(x, gi))
+	DistributedObject(prune(send_indices(x, gi)))
 }
-gen_indices <- function(i, accum_dims) NULL
-send_indices <- function(x, i) NULL
+accumulated_dims <- function(chunk_dims) UseMethod("accumulated_dims", chunk_dims)
+accumulated_dims.array <- function(chunk_dims) {
+	margins <- seq(length(dim(chunk_dims))))
+	lapply(margins, function(margin)
+		cumsum(apply(chunk_dims, margin,
+			     function(s) apply(s, margin, sum)))
+	)
+}
+gen_indices <- function(i, accum_dims) {
+	which.max(accum_dims[-1] < i < accum_dims) # something like this... maybe using that geographical routine?
+}
+send_indices <- function(x, gi) {
+	ChunkReferenceArray(mapply(function(x, gi) do.ccall(list('['),
+			           list(list(x, gi))), x, gi))
+}
 
 prune <- function(x) {
-	DistributedObject(chunkReferenceArray(x)[emerge(do.dcall(function(x) NROW(x) == 0L, list(x)))])
+	DistributedObject(chunkReferenceArray(x)[emerge(do.dcall(function(x) NROW(x) == 0L, list(x))), drop=FALSE])
 }
 
 align <- function(x, y) NULL
